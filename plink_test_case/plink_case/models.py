89ef508363393f8c5daee5bfb26ccdea5c9bb639
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-08-16 11:41:32 +0300

    Validation Model Changes

--- plink_test_case/plink_case/models.py
+++ plink_test_case/plink_case/models.py
@@ -3,10 +3,12 @@ from django.core.validators import RegexValidator
 
 PASSWORD_REGEX = RegexValidator(r'^[a-zA-Z0-9_]+$',
                                 'Only numbers, letters, underscores are allowed.')
+FIRST_NAME_REGEX = RegexValidator(r'^[a-zA-Z-]+$', 'Only letters, dashes are allowed.')
+LAST_NAME_REGEX = RegexValidator(r'^[a-zA-Z- ]+$', 'Only letters, dashes and spaces are allowed.')
 
 
 class ValidParametres(models.Model):
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=16, validators=[PASSWORD_REGEX])
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, validators=[FIRST_NAME_REGEX])
+    last_name = models.CharField(max_length=100, validators=[LAST_NAME_REGEX])
